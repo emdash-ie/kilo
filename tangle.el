04d0dbac811deba265f465d3fa459a3e89a157c7
Emily Bourke <undergroundquizscene@protonmail.com>
2023-10-19 23:26:38 +1300

    Add first unit tests
    I haven’t done unit tests in C before! Found this µnit library by
    googling and I like the look of it. So far I’ve just added a couple of
    tests for ~newRow~ which have helped me understand how it works. I’m
    trying to track down the bug that’s causing the last letters of each row
    not to be drawn, and I’m hoping adding tests will get me there.

--- tangle.el
+++ tangle.el
@@ -1,16 +1,16 @@
 (mapc 'org-babel-tangle-file
-      (--map (concat "source/" it)
-             (list "display.org"
-                   "editorRow.org"
-                   "edit.org"
-                   "fileData.org"
-                   "kibi.org"
-                   "lists.org"
-                   "makefile.org"
-                   "pane.org"
-                   "registers.org"
-                   "string.org"
-                   "tuple.org"
-                   "undo.org"
-                   "util.org"
-                   "zipperBuffer.org")))
+      (-concat (--map (concat "source/" it)
+                      (list "display.org"
+                            "editorRow.org"
+                            "edit.org"
+                            "fileData.org"
+                            "kibi.org"
+                            "lists.org"
+                            "pane.org"
+                            "registers.org"
+                            "string.org"
+                            "tuple.org"
+                            "undo.org"
+                            "util.org"
+                            "zipperBuffer.org"))
+               (list "makefile.org")))
